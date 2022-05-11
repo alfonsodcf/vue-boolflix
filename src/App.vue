9d17e6ef2f3a979fbe6c33909ee5635a8b526dfd
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-11 17:25:17 +0200

    Boolflix

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="App">
+   <h1 class="text-center display-6">Prova</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './styles/generals.scss';
+ 
+</style>
