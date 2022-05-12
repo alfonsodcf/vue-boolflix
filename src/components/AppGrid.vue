fb8f38221ca1ac65b70cf1c1bbaa9dc8e535fd57
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-12 11:42:55 +0200

    Appgrid-section

---
+++ src/components/AppGrid.vue
@@ -0,0 +1,31 @@
+<template>
+      <section>
+          sonola grid 
+          
+      </section>
+</template>
+
+<script>
+// import state from '../store.js'
+export default {
+    name:'AppGrid',
+    props:['movies'],
+    data(){
+        return{
+            
+        }
+    },
+    // computed: {
+    //     myasearch(){
+    //         return state.search
+    //     }
+    // },
+    methods:{
+       
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
