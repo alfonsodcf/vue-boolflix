8e76c0e1c59eb3ac91f9a0aa910cccbb38798c3f
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-12 12:36:12 +0200

    App: ul in gred

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,11 @@
   <div id="App">
     <header>
       <h1 class="text-center display-6">Prova</h1>
-      <search-bar @performSearch="search"/>
+      <search-bar @performSearch="search" :loader="loading"/>
     </header>
     <main>
-      
-      <ul>
-        <li v-for="(movie) in movies" :key="movie.id">
-          id: {{movie.id}}
-          titolo originale: {{movie.original_titolo}}
-          titolo: {{movie.title}}
-          lingua: {{movie.original_language}}
-          voto: {{movie.vote_average}} 
-  
-        </li>
-      </ul>
-      <app-grid/>
+      <app-grid :items="movies" title="Movies" :loader="loading"/>
+      <app-grid :items="series" title="Series" :loader="loadingSeries"/>
     </main>
    
   </div>
@@ -37,7 +27,10 @@ export default {
     return{
       apiKey: '1db971936b4a663499a5805602ee380c',
       apiPath: 'https://api.themoviedb.org/3/search/',
-      movies:[]
+      movies:[],
+      series:[],
+      loading: false,
+      loadingSeries: false,
     }
   },
   methods:{
@@ -45,6 +38,18 @@ export default {
       axios.get(this.apiPath+'movie', queryParams).then((res)=>{
         //console.log(res.data.results)
         this.movies = res.data.results;
+        this.loading = false;
+      }).catch((error)=>{
+        console.log(error);
+      })
+    },
+    getSeries(queryParams){
+      axios.get(this.apiPath+'tv', queryParams).then((res)=>{
+        //console.log(res.data.results)
+        this.series = res.data.results;
+        this.loadingSeries = false;
+      }).catch((error)=>{
+        console.log(error);
       })
     },
     search(text){
@@ -55,7 +60,10 @@ export default {
           query: text
         }
       }
+      this.loading = true;
+      this.loadingSeries = true;
       this.getMovies(queryParams)
+      this.getSeries(queryParams)
     }
   }
   //results
