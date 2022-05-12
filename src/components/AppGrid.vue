1d3c3a3c8c5d97e7247ffacb5bfaa6d1f65925ac
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-12 12:36:32 +0200

    grid: stampa ul

--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -1,15 +1,27 @@
 <template>
-      <section>
-          sonola grid 
-          
-      </section>
+    <section>
+        <h2>{{title}}</h2>
+        <ul>
+            <li v-for="(item) in items" :key="item.id">
+                id: {{item.id}}
+                titolo originale: {{item.original_titolo ? item.original_titolo : item.original_name}} <!-- {{item.original_titolo ? item.original_titolo : item.original_name}}-->
+                titolo: {{item.title ? item.title : item.name}}  <!-- {{item.title ? item.title : item.name}}-->
+                Lingua: {{item.original_language}} <!-- <img :src="require('./assets/images/'+item.original_language+'.jpg')" :alt="item.original_language"> -->
+                voto: {{item.vote_average}} 
+            </li>
+        </ul>
+    </section>
 </template>
 
 <script>
 // import state from '../store.js'
 export default {
     name:'AppGrid',
-    props:['movies'],
+    props:{
+        items: Array,
+        loader: Boolean,
+        title: String
+    },//['items','loader'], 
     data(){
         return{
             
