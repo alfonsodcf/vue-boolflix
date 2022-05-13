98ea6f0bf16bda8716c47866b38337e61fe1f9f9
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-13 10:40:47 +0200

    grid:hover

--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -2,20 +2,29 @@
     <section class="container">
         <h2>{{title}}</h2>
         <div class="row">
-            <div class="col col-6 col-md-4 col-lg-3 mb-3 " v-for="(item) in items" :key="item.id">
-                <h3>titolo: {{item.title ? item.title : item.name}}</h3>
-                <h4>titolo originale: {{item.original_titolo ? item.original_titolo : item.original_name}}</h4>
-                <span v-for="(n,index) in 5" :key="index">
-                    <i :class="n <=transformScale(item) ? 'fa-solid fa-star golden-star' : 'fa-solid fa-star empty-star'"></i>
-                </span>
-                <img class="flag" 
-                v-if="availableFlags.includes(item.original_language)" 
-                :src="require(`../assets/images/${item.original_language}.png`)" 
-                :alt="`bandiera ${item.original_language}`">
+            <div class="col col-6 col-md-4 col-lg-3 mb-3" v-for="(item) in items" :key="item.id">
+                <div class="copertina">
+
                 
-                <p v-else >{{item.original_language}}</p>
+                <img class="img-copertina" :src="image + item.poster_path" :alt="item.title ? item.title : item.name">
+
+                <div class="overlay">
+                    <h3>titolo: {{item.title ? item.title : item.name}}</h3>
+                    <h4>titolo originale: {{item.original_titolo ? item.original_titolo : item.original_name}}</h4>
+                    <span v-for="(n,index) in 5" :key="index">
+                        <i :class="n <=transformScale(item) ? 'fa-solid fa-star golden-star' : 'fa-solid fa-star empty-star'"></i>
+                    </span>
+                    
+                    <img class="flag" 
+                        v-if="availableFlags.includes(item.original_language)" 
+                        :src="require(`../assets/images/${item.original_language}.png`)" 
+                        :alt="`bandiera ${item.original_language}`">
                 
-                <img class="img-fluid" :src="image + item.poster_path" :alt="item.title ? item.title : item.name">
+                    <p v-else >{{item.original_language}}</p>
+                </div>
+                
+                
+            </div>  
             </div>
         </div>
        
@@ -69,6 +78,30 @@ export default {
 section{
     .col{
         border: 2px solid black;
+
+        .copertina{
+            position: relative ;
+            width: 300px;
+            .img-copertina{
+                width: 100%;
+                display: block;
+            }
+
+            .overlay{
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background: rgba(0, 0, 0, 0.6);
+                color: white;
+                opacity: 0;
+                &:hover{
+                    opacity: 1;
+                }
+            }
+        }
+       
     }
     .flag{
         width: 25px;
